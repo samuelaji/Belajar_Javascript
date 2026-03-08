66b93526ae0905bcb4d233eac34e85c1687fa605
Samuel Satria Aji Manurung - 245314008 <samuelsatria1084@gmail.com>
2026-03-08 23:43:07 +0700

    mencoba array

---
+++ array.js
@@ -0,0 +1,11 @@
+let mahasiswa = ["Samuel", "Aji", "Satria"];
+
+mahasiswa.push("Budi"); 
+mahasiswa.pop(); 
+
+mahasiswa.unshift("Agus"); 
+mahasiswa.shift(); 
+
+mahasiswa.splice(1, 0, "Saiful"); 
+
+console.log(mahasiswa);
