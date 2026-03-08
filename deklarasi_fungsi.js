cfa37ab8b3feaa8b82afc9da5be307cc317dd221
Samuel Satria Aji Manurung - 245314008 <samuelsatria1084@gmail.com>
2026-03-08 23:31:31 +0700

    mencoba fungsi

---
+++ deklarasi_fungsi.js
@@ -0,0 +1,9 @@
+function sapaUser(nama) {
+    return "Halo, selamat datang " + nama + "!";
+}
+console.log(sapaUser("Samuel"));
+
+const hitungLuasPersegi = function(sisi) {
+    return sisi * sisi;
+};
+console.log("Luas persegi: " + hitungLuasPersegi(5));
